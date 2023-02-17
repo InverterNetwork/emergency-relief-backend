6127e5f06948a1a358813de8f3055c968a130883
Bora Baloglu <bora.baloglu@opvs.io>
2023-02-18 00:28:40 +0300

    feat(*): add rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 import { OrganizationsModule } from '@src/modules/organizations/organizations.module';
 import { ProjectsModule } from '@src/modules/projects/projects.module';
 import { TransactionsModule } from '@src/modules/transactions/transactions.module';
 
 @Module({
-  imports: [OrganizationsModule, ProjectsModule, TransactionsModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 30,
+      limit: 100,
+    }),
+    OrganizationsModule,
+    ProjectsModule,
+    TransactionsModule,
+  ],
   controllers: [],
   providers: [],
 })
